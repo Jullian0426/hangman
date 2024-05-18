42bb07c7c362ecf9cdb877e4bcd8b594f7baeb23
Jullian Calkins <jullian0426@yahoo.com>
2024-05-18 09:00:42 -0500

    Add logic for initialization, guessing, and displaying

---
+++ lib/hangman.rb
@@ -0,0 +1,43 @@
+require 'set'
+
+class Hangman
+  DICTIONARY_PATH = 'google-10000-english-no-swears.txt'
+
+  attr_reader :secret_word, :guesses_left, :correct_guesses, :incorrect_guesses
+
+  def initialize
+    @secret_word = select_random_word
+    @guesses_left = 6
+    @correct_guesses = []
+    @incorrect_guesses = []
+  end
+
+  def select_random_word
+    words = File.readlines(DICTIONARY_PATH).map(&:chomp)
+    words.select { |word| word.length.between?(5, 12) }.sample
+  end
+
+  def guess(letter)
+    letter = letter.downcase
+    return if @correct_guesses.include?(letter) || @incorrect_guesses.include?(letter)
+
+    if @secret_word.include?(letter)
+      @correct_guesses << letter
+    else
+      @incorrect_guesses << letter
+      @guesses_left -= 1
+    end
+  end
+
+  def display_progress
+    @secret_word.chars.map { |char| @correct_guesses.include?(char) ? char : '_' }.join(' ')
+  end
+
+  def game_over?
+    @guesses_left <= 0 || !display_progress.include?('_')
+  end
+end
+
+# Start a new game
+game = Hangman.new
+puts "Secret word selected: #{game.secret_word}"
